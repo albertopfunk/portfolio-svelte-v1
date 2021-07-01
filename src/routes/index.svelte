a7c22be678a63a4d9e44d5d39c89b8c7f09864e5
Alberto-Funk <laptopreneur@gmail.com>
2021-07-01 01:52:14 -0700

    home page components - setting up sections and side nav -

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,22 @@
+<script>
+	import PageNav from "../components/home/PageNav.svelte";
+	import IntroSection from "../components/home/IntroSection.svelte";
+	import ProjectsSection from "../components/home/ProjectsSection.svelte";
+	import ContactSection from "../components/home/ContactSection.svelte";
+
+	let currentSection = null;
+</script>
+
 <svelte:head>
 	<title>Welcome</title>
 </svelte:head>
 
 <h1>Hello and welcome to my site!</h1>
+<PageNav {currentSection} />
+<IntroSection />
+<ProjectsSection />
+<ContactSection />
+
+<style>
+  
+</style>
