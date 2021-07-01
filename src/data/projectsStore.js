1d9d97db2a0a55040389950bcb1c947aed067aa9
Alberto-Funk <laptopreneur@gmail.com>
2021-07-01 04:07:42 -0700

    static data - svelte readable store for static project data

---
+++ src/data/projectsStore.js
@@ -0,0 +1,36 @@
+import { readable } from "svelte/store";
+
+export const projectSectionData = readable([
+	{
+		name: "Tech Profiles",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum perferendis ea, officiis, cum asperiores repellendus natus dicta tenetur inventore blanditiis ratione dolorem, quas eum perspiciatis placeat atque nihil. Assumenda, et!",
+		image:
+			"https://res.cloudinary.com/dy5hgr3ht/image/upload/c_scale,w_800/v1578652622/Portfolio/cao2za6cryixy6vlwnbt.webp",
+		slug: "tech-profiles"
+	},
+	{
+		name: "NomCue",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum perferendis ea, officiis, cum asperiores repellendus natus dicta tenetur inventore blanditiis ratione dolorem, quas eum perspiciatis placeat atque nihil. Assumenda, et!",
+		image:
+			"https://res.cloudinary.com/dy5hgr3ht/image/upload/c_scale,w_800/v1578652622/Portfolio/cao2za6cryixy6vlwnbt.webp",
+		slug: "nomcue"
+	},
+	{
+		name: "Portfolio",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum perferendis ea, officiis, cum asperiores repellendus natus dicta tenetur inventore blanditiis ratione dolorem, quas eum perspiciatis placeat atque nihil. Assumenda, et!",
+		image:
+			"https://res.cloudinary.com/dy5hgr3ht/image/upload/c_scale,w_800/v1578652622/Portfolio/cao2za6cryixy6vlwnbt.webp",
+		slug: "portfolio"
+	},
+	{
+		name: "Rollup Start Fresh",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum perferendis ea, officiis, cum asperiores repellendus natus dicta tenetur inventore blanditiis ratione dolorem, quas eum perspiciatis placeat atque nihil. Assumenda, et!",
+		image:
+			"https://res.cloudinary.com/dy5hgr3ht/image/upload/c_scale,w_800/v1578652622/Portfolio/cao2za6cryixy6vlwnbt.webp",
+		slug: "rollup-start-fresh"
+	}
+]);
