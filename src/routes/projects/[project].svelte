7542ef211cb5d0d3f3e551b2e018f8a907cdd012
Alberto-Funk <laptopreneur@gmail.com>
2021-07-01 22:24:27 -0700

    project page - using load function to get params - 404 pages wip

--- src/routes/projects/[project].svelte
+++ src/routes/projects/[project].svelte
@@ -1,5 +1,25 @@
+<script context="module">
+	export const load = ({ page }) => {
+		const project = page.params.project;
+		// check project matches or 404 page
+
+		return {
+			props: {
+				project
+			}
+		};
+	};
+</script>
+
+<script>
+	export let project;
+</script>
+
 <svelte:head>
 	<title>Project</title>
 </svelte:head>
 
-<h1>Project!</h1>
+<main>
+	<h1>Project!</h1>
+	<p>{project}</p>
+</main>
