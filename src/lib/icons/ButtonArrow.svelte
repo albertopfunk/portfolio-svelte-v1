f4e0cd1e39f142bb2e7f2246d08ad4c570bc0cad
Alberto-Funk <laptopreneur@gmail.com>
2021-07-03 03:39:50 -0700

    reusable component - trying to create svg component with dynamic styles -still wip, trying not to get too hacky and stay within svelte patterns

---
+++ src/lib/icons/ButtonArrow.svelte
@@ -0,0 +1,27 @@
+<script>
+	let styles = {
+		"icon-fill": "white",
+		"icon-height": "1.3rem",
+		"icon-width": "1.3rem",
+		"icon-rotate": "rotate(90deg)",
+	};
+
+	$: cssVarStyles = Object.entries(styles)
+		.map(([key, value]) => `--${key}:${value}`)
+		.join(";");
+</script>
+
+<svg style={cssVarStyles} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+	><path
+		d="M8.094 2.814L17.186 12l-9.092 9.186-1.244-1.207L14.829 12 6.85 4.021l1.244-1.207zM8.122 0L4 4l8 8-8 8 4.122 4L20 12 8.122 0z"
+	/></svg
+>
+
+<style>
+	svg {
+		fill: var(--icon-fill, currentColor);
+		width: var(--icon-width, 1rem);
+		height: var(--icon-height, 1rem);
+		transform: var(--icon-rotate, rotate(90deg));
+	}
+</style>
