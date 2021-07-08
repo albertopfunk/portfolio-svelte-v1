4d89ec90ef9e2da6f1be8df37cde8242ee2a525a
Alberto-Funk <laptopreneur@gmail.com>
2021-07-08 04:27:21 -0700

    contact section fix - using width 100 was causing an overflow scroll - left/right fixed it

--- src/lib/home/ContactSection.svelte
+++ src/lib/home/ContactSection.svelte
@@ -46,8 +46,9 @@
 	.mountains-container {
 		position: absolute;
 		bottom: 0;
+		left: 0;
+		right: 0;
 		z-index: 20;
-		width: 100%;
 		height: 10%;
 		background-color: var(--darker-violet-color);
 	}
