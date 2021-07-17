c57a18d312d0c3dfddd847ec20e01f0609290da3
Alberto-Funk <laptopreneur@gmail.com>
2021-07-17 03:56:59 -0700

    project store data wip - adding placeholder data for now

--- src/data/projectsStore.js
+++ src/data/projectsStore.js
@@ -5,7 +5,14 @@ export const projectSectionData = readable([
 		name: "Tech Profiles",
 		description:
 			"The first version of this app was my capstone project when I attended lambda school(link to github), it was a 5 week team project, each team had a PM who would review our PRs and merge. I learned more in those 5 weeks than 6 months self learning, building fully functioning web apps with a team and a deadline is such a great experience. I left the app as is and started version 2, which was renamed to tech profiles, I have been working on it on the side since then. It is a discovery app for people in tech, where they can create a portfolio for others to see.",
-		features: "",
+		features: [
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem",
+			"first feature example",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem"
+		],
 		tech: [
 			{
 				name: "react(cra)",
@@ -54,8 +61,35 @@ export const projectSectionData = readable([
 		name: "NomCue",
 		description:
 			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum perferendis ea, officiis, cum asperiores repellendus natus dicta tenetur inventore blanditiis ratione dolorem, quas eum perspiciatis placeat atque nihil. Assumenda, et!",
-		features: "",
-		tech: [{ name: "", description: "" }],
+		features: [
+			"first feature example lorem lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem"
+		],
+		tech: [
+			{
+				name: "AuthO",
+				description: "login/sign up, auto log in, silent authentication"
+			},
+			{
+				name: "Stripe",
+				description: "simple to implement, customizable, great docs"
+			},
+			{
+				name: "Google places",
+				description: "locations search and Gio location"
+			},
+			{
+				name: "Netlify",
+				description: "big community, simple to deploy"
+			},
+			{
+				name: "supabase/vercel",
+				description: "big community, simple to deploy"
+			}
+		],
 		siteUrl: "nomcue.com",
 		githubUrl: "",
 		mediaUrl:
@@ -66,8 +100,35 @@ export const projectSectionData = readable([
 		name: "Portfolio",
 		description:
 			"I LOVE 80s retro themes(blade runner 2049 is my favorite movie). I see too many portfolios very similar, doing the same animations and similar layouts. I did not want to follow that path, my portfolio represents me and what I love",
-		features: "",
-		tech: [{ name: "", description: "" }],
+		features: [
+			"first feature example lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem"
+		],
+		tech: [
+			{
+				name: "AuthO",
+				description: "login/sign up, auto log in, silent authentication"
+			},
+			{
+				name: "Stripe",
+				description: "simple to implement, customizable, great docs"
+			},
+			{
+				name: "Google places",
+				description: "locations search and Gio location"
+			},
+			{
+				name: "Netlify",
+				description: "big community, simple to deploy"
+			},
+			{
+				name: "supabase/vercel",
+				description: "big community, simple to deploy"
+			}
+		],
 		siteUrl: "albertopfunk",
 		githubUrl: "",
 		mediaUrl:
@@ -78,8 +139,34 @@ export const projectSectionData = readable([
 		name: "Rollup Start Fresh",
 		description:
 			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum perferendis ea, officiis, cum asperiores repellendus natus dicta tenetur inventore blanditiis ratione dolorem, quas eum perspiciatis placeat atque nihil. Assumenda, et!",
-		features: "",
-		tech: [{ name: "", description: "" }],
+		features: [
+			"first feature example lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem lorem",
+			"first feature example lorem lorem"
+		],
+		tech: [
+			{
+				name: "AuthO",
+				description: "login/sign up, auto log in, silent authentication"
+			},
+			{
+				name: "Stripe",
+				description: "simple to implement, customizable, great docs"
+			},
+			{
+				name: "Google places",
+				description: "locations search and Gio location"
+			},
+			{
+				name: "Netlify",
+				description: "big community, simple to deploy"
+			},
+			{
+				name: "supabase/vercel",
+				description: "big community, simple to deploy"
+			}
+		],
 		siteUrl: "",
 		githubUrl: "",
 		mediaUrl:
