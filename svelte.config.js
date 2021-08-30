2bdb7cda04cad5dd39828d0d27004ef4c74e86a4
Alberto Preciado <albertopfunk@gmail.com>
2021-08-30 07:40:05 +0000

    netlify adapter - leaving as esm to keep config as .js - ignoring build/functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-const sveltePreprocess = require('svelte-preprocess');
-const adapter = require('@sveltejs/adapter-netlify');
+import sveltePreprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
