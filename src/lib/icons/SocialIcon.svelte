b9ca9fe0f61f1e8dd26916c39964c683eb2c1455
Alberto Preciado <albertopfunk@gmail.com>
2021-09-07 08:08:46 +0000

    Icon reuse - the social icons I am using are reusing the same styles so I created - a main social icon svg with a slot so other - icons can pass in the main svg path - this allows me to better reuse the icons with different styles

---
+++ src/lib/icons/SocialIcon.svelte
@@ -0,0 +1,55 @@
+<script>
+	export let hidden = false;
+	export let lightViolet = false;
+	export let redViolet = false;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	aria-hidden={hidden}
+	class:lightViolet
+	class:redViolet
+>
+	<defs>
+		<linearGradient id="light-violet-gradient" gradientTransform="rotate(90)">
+			<stop offset="0%" stop-color="rgba(var(--lighter-violet-value), 0.7)" />
+			<stop offset="75%" stop-color="rgba(var(--light-violet-value), 0.8)" />
+		</linearGradient></defs
+	>
+	<defs>
+		<linearGradient id="violet-gradient" gradientTransform="rotate(90)">
+			<stop offset="0%" stop-color="rgba(var(--light-violet-value), 0.7)" />
+			<stop offset="75%" stop-color="rgba(var(--violet-value), 0.9)" />
+		</linearGradient></defs
+	>
+	<slot />
+</svg>
+
+<style>
+	svg {
+		fill: var(--lighter-violet-color);
+	}
+
+	svg:hover {
+		fill: var(--red-color);
+	}
+
+	svg.lightViolet {
+		fill: url("#light-violet-gradient");
+	}
+
+	svg.lightViolet:hover {
+		fill: url("#violet-gradient");
+	}
+
+	svg.redViolet {
+		fill: var(--lighter-violet-color);
+		filter: drop-shadow(0 0 10px rgba(var(--red-value), 0.8));
+	}
+
+	svg.redViolet:hover {
+		fill: var(--red-color);
+		filter: drop-shadow(0 0 10px rgba(var(--red-value), 0.5));
+	}
+</style>
